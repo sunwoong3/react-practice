59ebb1348194f74c5bfc0b8a71773e51c66bc859
sunwoong3 <sunw339@gmail.com>
2021-10-25 15:41:04 +0900

    비밀정보보호

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
-const express = require('express'); //express모듈가져옴
+const express = require("express"); //express모듈가져옴
 const app = express(); //function을 이용해서 express 앱 만들기
 const port = 5000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
 
-const mongoose = require('mongoose');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
 mongoose
-	.connect('mongodb+srv://sunwoo:poiu8523@sunwoo.76xpd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-	.then(() => console.log('MongoDB Connected...'))
-	.catch((err) => console.log(err));
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('안녕하세요')); // 루트디렉토리에 오면 hello world 출력
+app.get("/", (req, res) => res.send("안녕하세요gg")); // 루트디렉토리에 오면 hello world 출력
+
+app.post("/register", (req, res) => {
+  //회원가입하때 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터베이스에 넣어준다
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ succsee: false, err });
+    return res.status(200).json({ succsee: true });
+  }); //save는 몽고db의 메소드, 정보들이 user모델에 저장
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); //포트번호에서 실행
--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true, //중간에 띄어쓰기 없애주는역할
-        unique:1 //이메일은 고유해야되니까 유니크
-    },
-    password: {
-        type: String,
-        maxlength:5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    }, 
-    role: { //관리자인지 일반유저인지 등 구분해주고싶을때
-        type: Number,
-        default: 0 //입력하지않을경우 저절로 디폴트값메겨짐
-    },
-    image: String,//꼭 obj타입으로 작성하지 않아도됨
-    token: { //나중에 유효성검사할때 사용
-        type:String 
-    },
-    tokenExp: { //토큰유효기간
-        type:Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //중간에 띄어쓰기 없애주는역할
+    unique: 1, //이메일은 고유해야되니까 유니크
+  },
+  password: {
+    type: String,
+    maxlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    //관리자인지 일반유저인지 등 구분해주고싶을때
+    type: Number,
+    default: 0, //입력하지않을경우 저절로 디폴트값메겨짐
+  },
+  image: String, //꼭 obj타입으로 작성하지 않아도됨
+  token: {
+    //나중에 유효성검사할때 사용
+    type: String,
+  },
+  tokenExp: {
+    //토큰유효기간
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User', userSchema) //모델로 스키마를 감싸줌
+const User = mongoose.model("User", userSchema); //모델로 스키마를 감싸줌
 
-module.exports = { User } // 다른곳에서도 쓸수있게 export해줌
+module.exports = { User }; // 다른곳에서도 쓸수있게 export해줌
